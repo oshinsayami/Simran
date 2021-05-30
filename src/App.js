521d17cb3632398b03d1cfc9a587e9a7af6ddfba
Simran <mdrsimran@gmail.com>
2021-05-30 10:03:10 +0545

    add home router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="app">
-      Hello
+    <Router>
+      <div className="app">
+        <Switch>
+        <Route exact path="/" component={Home} />
+        </Switch>
+      
     </div>
+    </Router>
+    
   );
 }
 
