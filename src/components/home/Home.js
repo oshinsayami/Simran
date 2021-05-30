b6bb1dd92f7867dec14349ef079896bfad3441cd
Simran <mdrsimran@gmail.com>
2021-05-30 21:09:19 +0545

    home component and styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import './home.css'
+import women from './../../img/women.png'
+
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home">
+        <img className="img1" src={women} alt="img1"></img>
+            <div className="container">
+                <h1 className="hello">Hi!</h1>
+                <h1>Welcome to my website!!</h1>
+            </div>
+
         </div>
     )
 }
+
+export default Home;
