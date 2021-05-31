5651725c375f10755aef5fd60f95a1f76abafd76
Simran <mdrsimran@gmail.com>
2021-05-31 16:02:13 +0545

    add about me section and style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Topbar from './components/topbar/Topbar';
-
+import Contact from './components/contact/Contact';
+import Intro from './components/intro/Intro';
+import Project from './components/projects/Project';
+import Home from './components/home/Home'
+import './app.css'
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Topbar/>
-      Hello
-    </div>
+        <Topbar />
+        <div className="sections">
+          <Home/>
+          <Intro />
+          <Project />
+          <Contact />
+        </div>
+      </div>
     </Router>
     
   );
---
+++ src/components/intro/Intro.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./intro.css"
+import PageHeader from './../pageHeader/PageHeader'
+import me from './../../img/me.jpg'
+
+export default function intro() {
+    return (
+        <div className="intro">
+            <PageHeader title={'About Me'} />
+            <div className="container">
+                <div className="text">
+                    <h2> Hello! I'm Simran Manandhar :)</h2>
+                    <p>
+                        I'm an enthusiastic, trilingual self-starter experienced in Ruby on Rails, React, and JavaScript based programming with professional experience in Electrical Engineering. I've drive and passion to create measurable results, through continuous improvement, and successful collaborations. I possess solid foundation of web development and the ability to conceptualize solutions to complex problems. And I thrive when challenged to create solutions, develop prototypes and be a successful team member.
+                    </p>
+
+                </div>
+                <div className="photo">
+                <img className="img" src={me} alt="img"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
