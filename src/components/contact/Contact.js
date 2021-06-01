6004af331c3f06aa975c9dd8052f9d13c58ad4f8
Simran <mdrsimran@gmail.com>
2021-06-01 10:54:02 +0545

    add icons for contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./contact.css"
+import { FaYoutube, FaMediumM, FaGithub, FaLinkedin } from "react-icons/fa";
+
+const handleURL = (url) => {
+    return ( )=> window.open(url,"_blank")
+}
 
 export default function Contact() {
     return (
         <div className="contact">
-            
+            <FaYoutube color='white' size='30px' style={{ padding: '1%' }} onClick={handleURL('https://www.youtube.com/channel/UCEC0yt7WB6ZW9_KA8Fm94GQ') } />
+            <FaMediumM color='white' size='30px' style={{ padding: '1%' }} onClick={handleURL('https://mdrsimran.medium.com/') } />
+            <FaGithub color='white' size='30px' style={{ padding: '1%' }} onClick={handleURL('https://github.com/oshinsayami') } />
+            <FaLinkedin color='white' size='30px' style={{ padding: '1%' }} onClick={handleURL('https://www.linkedin.com/in/simran-manandhar/') } />
         </div>
     )
 }
